32ed1c1136c97353eeab2646d1879775d65a48a5
abdallahmostafa281 <abdallahmostafa281@gmail.com>
2024-03-25 00:49:24 +0200

    add floating action button

--- lib/widgets/custom_item.dart
+++ lib/widgets/custom_item.dart
@@ -5,52 +5,55 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCD7B),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                "Bulid your Career with Abdallah Mostafa",
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCD7B),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withOpacity(0.5),
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  "Bulid your Career with Abdallah Mostafa",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 24,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 24,
-            ),
-            child: Text(
-              'May21 , 2022',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
-            ),
-          )
-        ]),
+              child: Text(
+                'May21 , 2022',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 16),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
