5ac7c3e5aa273b89cfbe3479664f9a1e47027d1e
abdallahmostafa281 <abdallahmostafa281@gmail.com>
2024-03-24 18:14:42 +0200

    create custom item note and add font family

---
+++ lib/widgets/custom_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCD7B),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                "Bulid your Career with Abdallah Mostafa",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Text(
+              'May21 , 2022',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -13,7 +14,11 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          CustomAppBar()
+          CustomAppBar(),
+          SizedBox(
+            height: 16,
+          ),
+          NoteItem(),
         ],
       ),
     );
