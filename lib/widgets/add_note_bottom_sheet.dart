e4674fe5ac0eeeaa62ff7a8c581e8918180f4a38
abdallahmostafa281 <abdallahmostafa281@gmail.com>
2024-03-28 06:22:20 +0200

    add single child scroll view to bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -9,28 +9,33 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'Titel',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxline: 5,
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomBottom(
-            text: 'Add',
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'Titel',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxline: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomBottom(
+              text: 'Add',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
